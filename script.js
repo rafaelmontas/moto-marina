1efbc6f20e659379c6e87b6b4fcea7a4940fed34
Rafael Montas <rafaelmontas1@gmail.com>
2021-12-15 19:27:35 -0400

    Add basic tour cards section

--- script.js
+++ script.js
@@ -4,5 +4,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
   burgerMenu.addEventListener('click', () => {
     const drawer = document.getElementById('mobile-drawer').classList.toggle('open')
     const body = document.body.classList.toggle('noscroll')
+    const header = document.getElementById('header')
+    const headerAction = header.classList.toggle('no-after')
   })
 });
